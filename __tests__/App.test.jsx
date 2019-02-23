ac561f0faa6cb301b01962134b7a4ab86285dd85
sergei.fidler <fidler.sergei@gmail.com>
2019-02-23 12:26:29 +0400

    fixes configs

--- __tests__/App.test.jsx
+++ __tests__/App.test.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { mount } from "enzyme";
 
-import App from "../../src/components/App";
+import App from "../src/components/App";
 
 describe("test <App />", () => {
   it("render default tabs", () => {
